cdaed03a92a086d7befe822cd32e9a2f94d6758d
FuryFighter <klotzcraftlp@googlemail.com>
2020-01-09 14:08:36 +0100

    4 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent ],
-  providers: [],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
+  providers: [HeroService, MessageService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
