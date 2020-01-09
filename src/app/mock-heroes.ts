2582c74a38c340ef5627905a2fbfb71dd74ac258
FuryFighter <klotzcraftlp@googlemail.com>
2020-01-09 15:50:00 +0100

    4 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Ironman' },
-  { id: 12, name: 'NicoNicoNii' },
-  { id: 13, name: 'Patrick Starfish' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magenta Man' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Ironman', detail: "" },
+  { id: 12, name: 'NicoNicoNii', detail: "" },
+  { id: 13, name: 'Patrick Starfish', detail: "Heslig und ein KPOP Fan/Blink/Insomnia" },
+  { id: 14, name: 'Celeritas', detail: "" },
+  { id: 15, name: 'Magenta Man', detail: "" },
+  { id: 16, name: 'RubberMan', detail: "" },
+  { id: 17, name: 'Dynama', detail: "" },
+  { id: 18, name: 'Dr IQ', detail: "" },
+  { id: 19, name: 'Magma', detail: "" },
+  { id: 20, name: 'Tornado', detail: "" }
 ];
