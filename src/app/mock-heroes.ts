e9eb694dd61da16c9dd6c25750c2ecd32ce0f759
FuryFighter <klotzcraftlp@googlemail.com>
2020-01-10 11:09:44 +0100

    4 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,10 +2,10 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   
-  { id: 2, name: 'Jisoo', birthday: "10.07.1999", height: 0, ethnicity: "", languages: "", member_since: "" },
-  { id: 3, name: 'Jennie',birthday: "", height: 0, ethnicity: "", languages: "", member_since: ""  },
-  { id: 4, name: 'Lisa',birthday: "", height: 0, ethnicity: "", languages: "", member_since: "" },
-  { id: 5, name: 'Rose',birthday: "", height: 0, ethnicity: "", languages: "", member_since: "" },
+  { id: 2, name: 'Jisoo', birthday: "3. Januar 1995", height: 0, ethnicity: "", languages: "", member_since: "" },
+  { id: 3, name: 'Jennie',birthday: "16. Januar 1996", height: 0, ethnicity: "", languages: "", member_since: ""  },
+  { id: 4, name: 'Rosé',birthday: "11. Februar 1997", height: 0, ethnicity: "", languages: "", member_since: "" },
+  { id: 5, name: 'Lisa',birthday: "27. März 1997", height: 0, ethnicity: "", languages: "", member_since: "" },
   { id: 6, name: 'JiU',birthday: "", height: 0, ethnicity: "", languages: "", member_since: "" },
   { id: 7, name: 'SuA',birthday: "", height: 0, ethnicity: "", languages: "", member_since: "" },
   { id: 8, name: 'Siyeon',birthday: "", height: 0, ethnicity: "", languages: "", member_since: "" },
